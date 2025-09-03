4b325cb0855e65aa823fbae978a7edf1070a98f8
matt higg <mhh129@gmail.com>
2025-09-02 21:01:00 -0400

    Update simulate_battle method to randomize order when both creatures have same speed

--- app/modules/battle_simulation.py
+++ app/modules/battle_simulation.py
@@ -1,4 +1,5 @@
 import torch
+import random
 from app.config import ACTION_NAMES, CONFIG
 from app.modules.logging_utils import append_battle_log
 from app.modules.utils import choose_action, create_state
@@ -9,10 +10,17 @@ def simulate_battle(creature_A, creature_B, epoch, batch_size, epsilons):
   creature_B.hp, creature_B.energy = creature_B.max_hp, creature_B.max_energy
   battle_log = []
   rewards = {creature_A.name: 0.0, creature_B.name: 0.0}
-  turn_order = sorted([creature_A, creature_B], key=lambda c: c.speed, reverse=True)
   zero = torch.zeros(len(ACTION_NAMES))
 
   for tick in range(batch_size):
+    # Determine turn order each tick
+    creatures = [creature_A, creature_B]
+    creatures.sort(key=lambda c: c.speed, reverse=True)
+    # Randomize if speeds tie
+    if creatures[0].speed == creatures[1].speed and random.random() < 0.5:
+      creatures[0], creatures[1] = creatures[1], creatures[0]
+    turn_order = creatures
+
     for creature in turn_order:
       opponent = creature_B if creature is creature_A else creature_A
       epsilon = epsilon_A if creature is creature_A else epsilon_B
@@ -37,8 +45,18 @@ def simulate_battle(creature_A, creature_B, epoch, batch_size, epsilons):
         reward = action_fn(opponent)
 
       rewards[creature.name] += reward
-      append_battle_log(epoch, tick, creature if opponent.is_alive() else opponent, opponent if opponent.is_alive() else creature, battle_log, action_name if opponent.is_alive() else '*KNOCKOUT*', probs, action_idx, reward)
+      append_battle_log(
+        epoch, tick,
+        creature if opponent.is_alive() else opponent,
+        opponent if opponent.is_alive() else creature,
+        battle_log,
+        action_name if opponent.is_alive() else '*KNOCKOUT*',
+        probs,
+        action_idx,
+        reward
+      )
 
+  # Final win/loss rewards
   reward_win_A = creature_A.reward_config.get('win', CONFIG['reward_win'])
   reward_lose_A = creature_A.reward_config.get('lose', CONFIG['reward_lose'])
   reward_win_B = creature_B.reward_config.get('win', CONFIG['reward_win'])
@@ -54,6 +72,7 @@ def simulate_battle(creature_A, creature_B, epoch, batch_size, epsilons):
     rewards[creature_A.name] += reward_lose_A
     winner = creature_B.name
 
+  # Apply final rewards to last entries
   for entry in reversed(battle_log):
     if entry['creature'] == creature_A.name:
       entry['reward'] += rewards[creature_A.name]
